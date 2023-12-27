bd9004d18854d27c686fc384b248a0a0b66d5066
Federico Brigante <me@fregante.com>
2023-12-27 17:36:05 +0800

    Add tests for v3

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import {test, assert} from 'vitest';
-import manifestJson from './test/fixtures/manifest.json' with {type: 'json'};
+import {test, describe, assert} from 'vitest';
+import manifest2 from './test/fixtures/manifest-v2.json' with {type: 'json'};
+import manifest3 from './test/fixtures/manifest-v3.json' with {type: 'json'};
 import atStart from './test/fixtures/reported-at-start.json' with {type: 'json'};
 import afterAddition from './test/fixtures/reported-after-addition.json' with {type: 'json'};
 import {
@@ -9,8 +10,6 @@ import {
 	isUrlPermittedByManifest,
 	dropOverlappingPermissions,
 } from './index.js';
-
-const manifest = manifestJson as chrome.runtime.ManifestV2;
 
 // AVA compatibility layer
 const t = {
@@ -21,142 +20,148 @@ const t = {
 	},
 };
 
-test('normalizeManifestPermissions', () => {
-	t.deepEqual(normalizeManifestPermissions(manifest), {
-		origins: [
-			'https://github.com/*',
-			'https://api.github.com/*',
-			'https://gist.github.com/*',
-			'https://ghe.github.com/*',
-		],
-		permissions: [
-			'storage',
-			'contextMenus',
-			'activeTab',
-			'alarms',
-			'devtools',
-		],
+describe.each([
+	['manifest v2', manifest2 as chrome.runtime.ManifestV2],
+	['manifest v3', manifest3 as chrome.runtime.ManifestV3],
+])('%s', (_, manifest) => {
+	test('normalizeManifestPermissions', () => {
+		t.deepEqual(normalizeManifestPermissions(manifest), {
+			origins: [
+				'https://github.com/*',
+				'https://api.github.com/*',
+				'https://gist.github.com/*',
+				'https://ghe.github.com/*',
+			],
+			permissions: [
+				'storage',
+				'contextMenus',
+				'activeTab',
+				'alarms',
+				'devtools',
+			],
+		});
 	});
-});
 
-test('extractAdditionalPermissions at install', () => {
-	t.deepEqual(extractAdditionalPermissions(atStart, {manifest}), {
-		origins: [],
-		permissions: [],
+	test('extractAdditionalPermissions at install', () => {
+		t.deepEqual(extractAdditionalPermissions(atStart, {manifest}), {
+			origins: [],
+			permissions: [],
+		});
 	});
-});
 
-test('extractAdditionalPermissions after added permissions', () => {
-	t.deepEqual(extractAdditionalPermissions(afterAddition, {manifest}), {
-		origins: [
-			'https://*.github.com/*',
-			'https://git.example.com/*',
-		],
-		permissions: [
-			'someOtherPermission',
-		],
+	test('extractAdditionalPermissions after added permissions', () => {
+		t.deepEqual(extractAdditionalPermissions(afterAddition, {manifest}), {
+			origins: [
+				'https://*.github.com/*',
+				'https://git.example.com/*',
+			],
+			permissions: [
+				'someOtherPermission',
+			],
+		});
 	});
-});
 
-test('extractAdditionalPermissions after added permissions, loose origin check', () => {
-	t.deepEqual(extractAdditionalPermissions(afterAddition, {manifest, strictOrigins: false}), {
-		origins: [
-			'https://git.example.com/*',
-		],
-		permissions: [
-			'someOtherPermission',
-		],
+	test('extractAdditionalPermissions after added permissions, loose origin check', () => {
+		t.deepEqual(extractAdditionalPermissions(afterAddition, {manifest, strictOrigins: false}), {
+			origins: [
+				'https://git.example.com/*',
+			],
+			permissions: [
+				'someOtherPermission',
+			],
+		});
 	});
-});
 
-test('dropOverlappingPermissions', () => {
-	t.deepEqual(dropOverlappingPermissions({
-		origins: [
-			'https://*.example.com/*',
-			'<all_urls>',
-			'https://fregante.com/*',
-			'*://*/*',
-		],
-	}), {
-		origins: [
-			'<all_urls>',
-		],
-	}, '<all_urls> should catch all');
+	test('dropOverlappingPermissions', () => {
+		t.deepEqual(dropOverlappingPermissions({
+			origins: [
+				'https://*.example.com/*',
+				'<all_urls>',
+				'https://fregante.com/*',
+				'*://*/*',
+			],
+		}), {
+			origins: [
+				'<all_urls>',
+			],
+		}, '<all_urls> should catch all');
 
-	t.deepEqual(dropOverlappingPermissions({
-		origins: [
-			'https://*.example.com/*',
-			'*://*/*',
-			'https://fregante.com/*',
-		],
-	}), {
-		origins: ['*://*/*'],
-	}, '*://*/* should catch all');
+		t.deepEqual(dropOverlappingPermissions({
+			origins: [
+				'https://*.example.com/*',
+				'*://*/*',
+				'https://fregante.com/*',
+			],
+		}), {
+			origins: ['*://*/*'],
+		}, '*://*/* should catch all');
 
-	t.deepEqual(dropOverlappingPermissions({
-		origins: [
-			'http://*.example.com/*',
-			'https://*/*',
-			'https://fregante.com/*',
-		],
-	}), {
-		origins: [
-			'http://*.example.com/*',
-			'https://*/*',
-		],
-	}, 'https://*/* should drop all other https origins');
+		t.deepEqual(dropOverlappingPermissions({
+			origins: [
+				'http://*.example.com/*',
+				'https://*/*',
+				'https://fregante.com/*',
+			],
+		}), {
+			origins: [
+				'http://*.example.com/*',
+				'https://*/*',
+			],
+		}, 'https://*/* should drop all other https origins');
 
-	t.deepEqual(dropOverlappingPermissions({
-		origins: [
-			'https://git.example.com/*',
-			'https://*.example.com/*',
-			'https://example.com/*',
-			'https://fregante.com/*',
-		],
-	}), {
-		origins: [
-			'https://*.example.com/*',
-			'https://fregante.com/*',
-		],
-	}, 'A subdomain star should drop all other same-domain origins');
+		t.deepEqual(dropOverlappingPermissions({
+			origins: [
+				'https://git.example.com/*',
+				'https://*.example.com/*',
+				'https://example.com/*',
+				'https://fregante.com/*',
+			],
+		}), {
+			origins: [
+				'https://*.example.com/*',
+				'https://fregante.com/*',
+			],
+		}, 'A subdomain star should drop all other same-domain origins');
 
-	t.deepEqual(dropOverlappingPermissions({
-		origins: [
-			'https://git.example.com/*',
-			'https://git.example.com/fregante/*',
-		],
-	}), {
-		origins: [
-			'https://git.example.com/*',
-		],
-	}, 'A pathname star should drop all other same-origin origins');
-});
+		t.deepEqual(dropOverlappingPermissions({
+			origins: [
+				'https://git.example.com/*',
+				'https://git.example.com/fregante/*',
+			],
+		}), {
+			origins: [
+				'https://git.example.com/*',
+			],
+		}, 'A pathname star should drop all other same-origin origins');
+	});
 
-// This is identical to the internal normalizeManifestPermissions, which is already tested
-test('extractAdditionalPermissions', () => {
-	t.pass();
-});
+	// This is identical to the internal normalizeManifestPermissions, which is already tested
+	test('extractAdditionalPermissions', () => {
+		t.pass();
+	});
 
-test('isUrlPermittedByManifest ', () => {
-	t.is(isUrlPermittedByManifest('https://ghe.github.com/*', manifest), true);
-	t.is(isUrlPermittedByManifest('https://github.com/contacts/', manifest), true);
-	t.is(isUrlPermittedByManifest('https://other.github.com/contacts/', manifest), false);
-	t.is(isUrlPermittedByManifest('https://example.com/contacts/', {
-		content_scripts: [
-			{
-				matches: [
-					'https://*/*',
-				],
-			},
-		],
-	} as chrome.runtime.Manifest), true);
-	t.is(isUrlPermittedByManifest('http://insecure.com/', {
-		content_scripts: [
-			{
-				matches: [
-					'https://*/*',
-				],
-			},
-		],
-	} as chrome.runtime.Manifest), false);
-});
+	test('isUrlPermittedByManifest ', () => {
+		t.is(isUrlPermittedByManifest('https://ghe.github.com/*', manifest), true);
+		t.is(isUrlPermittedByManifest('https://github.com/contacts/', manifest), true);
+		t.is(isUrlPermittedByManifest('https://other.github.com/contacts/', manifest), false);
+		t.is(isUrlPermittedByManifest('https://example.com/contacts/', {
+			content_scripts: [
+				{
+					matches: [
+						'https://*/*',
+					],
+				},
+			],
+		} as chrome.runtime.Manifest), true);
+		t.is(isUrlPermittedByManifest('http://insecure.com/', {
+			content_scripts: [
+				{
+					matches: [
+						'https://*/*',
+					],
+				},
+			],
+		} as chrome.runtime.Manifest), false);
+	});
+},
+);
